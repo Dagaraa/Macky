e390ab2310b30beab475c8195c6b51a4ab5e4b37
Dagaraa <120491681+Dagaraa@users.noreply.github.com>
2023-11-30 18:11:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.getElementById("contactButton").addEventListener("click", function() {
-    const templateParams = {
-        to_email: 'your_email@gmail.com',
-        message: 'Someone pressed the button!',
-    };
 
-    emailjs.send('default_service', templateID, templateParams)
-        .then(function(response) {
-            console.log('Email sent successfully:', response);
-            alert('Email sent successfully');
-        }, function(error) {
-            console.error('Error sending email:', error);
-            alert('Error sending email');
-        });
-});
